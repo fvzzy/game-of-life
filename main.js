4d1943d115db870832ac597b4cfb6f8783d736ce
Farhan Rahman <fvzzy@users.noreply.github.com>
2022-01-07 18:04:42 +1100

    Create the basic game with some configurable options

--- main.js
+++ main.js
@@ -1,35 +1,27 @@
-const GRID_SIZE = 5; // the number of cells across and down
-const CELL_SPACING = 3; // spacing in pixels around each cell
-const CELL_COLOR = "black";
-const BACKGROUND_COLOR = "white";
-
-const input = [
-  [0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0],
-  [0, 1, 1, 1, 0],
-  [0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0],
-];
-
-const initCanvas = (canvas, bgColor, cellColor, cellSpacing) => {
+const initPage = (cellColor) => {
   const body = document.querySelector("body");
   body.style.background = cellColor;
+};
 
+const resetCanvas = (canvas, bgColor, cellSpacing) => {
   const ctx = canvas.getContext("2d");
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.fillStyle = bgColor;
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   canvas.style.border = `${cellSpacing}px solid ${bgColor}`;
 };
 
-const printCells = (canvas, grid, gridSize, color, spacing) => {
+const paintCells = (canvas, input, gridSize, color, spacing) => {
   const ctx = canvas.getContext("2d");
   const cellW = canvas.width / gridSize;
   const cellH = canvas.height / gridSize;
 
   ctx.fillStyle = color;
-  for (let row = 0; row < grid.length; row++) {
-    for (let col = 0; col < grid[0].length; col++) {
-      if (grid[row][col] === 1) {
+  for (let row = 0; row < input.length; row++) {
+    for (let col = 0; col < input[0].length; col++) {
+      // update this so we only process the "on" cells,
+      // no need to iterate through the others
+      if (input[row][col]) {
         ctx.fillRect(
           cellW * col + spacing,
           cellH * row + spacing,
@@ -41,8 +33,78 @@ const printCells = (canvas, grid, gridSize, color, spacing) => {
   }
 };
 
-const canvas = document.querySelector("canvas#grid");
-initCanvas(canvas, BACKGROUND_COLOR, CELL_COLOR, CELL_SPACING);
-// for (let time = 0; time < 10; time++) {
-printCells(canvas, input, GRID_SIZE, CELL_COLOR, CELL_SPACING);
-// }
+const drawScene = (options, input) => {
+  const { canvas, gridSize, backgroundColor, cellColor, cellSpacing } = options;
+  resetCanvas(canvas, backgroundColor, cellSpacing);
+  paintCells(canvas, input, gridSize, cellColor, cellSpacing);
+};
+
+const countNeighbours = (grid, cellX, cellY) => {
+  const neighbours = [
+    [-1, -1],
+    [0, -1],
+    [1, -1],
+    [1, 0],
+    [1, 1],
+    [0, 1],
+    [-1, 1],
+    [-1, 0],
+  ];
+  let count = 0;
+  for (const n of neighbours) {
+    const [nX, nY] = [cellX + n[0], cellY + n[1]];
+    if (nX < 0 || nX >= grid[0].length || nY < 0 || nY >= grid.length) continue;
+    if (grid[nY][nX]) count += 1;
+  }
+  return count;
+};
+
+const nextInput = (input) => {
+  let result = Array(input.length)
+    .fill()
+    .map(() => Array(input[0].length).fill(0));
+
+  for (let y = 0; y < input.length; y++) {
+    for (let x = 0; x < input[0].length; x++) {
+      const neighbours = countNeighbours(input, x, y);
+      if (input[y][x] === 1) {
+        result[y][x] = [2, 3].includes(neighbours) ? 1 : 0;
+      } else if (neighbours === 3) {
+        result[y][x] = 1;
+      }
+    }
+  }
+  return result;
+};
+
+const play = (options) => {
+  const { cellColor, speed } = options;
+  let { input } = options;
+  const delay = Math.max(1000 / speed, 100);
+
+  initPage(cellColor);
+  drawScene(options, input);
+
+  setInterval(() => {
+    input = nextInput(input);
+    drawScene(options, input);
+  }, delay);
+};
+
+const options = {
+  canvas: document.querySelector("canvas#grid"),
+  gridSize: 5, // the number of cells across and down
+  cellSpacing: 3, // spacing in pixels around each cell
+  cellColor: "black",
+  backgroundColor: "white",
+  speed: 1, // setting from 1-10
+  input: [
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+  ],
+};
+
+play(options);

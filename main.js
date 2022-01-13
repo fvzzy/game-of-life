a64f9dc84420218c560eb71335857106ac6d163b
Farhan Rahman <fvzzy@users.noreply.github.com>
2022-01-13 12:54:19 +1100

    Make the canvas fill the screen

--- main.js
+++ main.js
@@ -1,40 +1,39 @@
-const initPage = (cellColor) => {
-  const body = document.querySelector("body");
-  body.style.background = cellColor;
+const stretchCanvas = (canvas, cellSize) => {
+  const { clientWidth, clientHeight } = document.documentElement;
+  canvas.width = Math.floor(clientWidth / cellSize) * cellSize;
+  canvas.height = Math.floor(clientHeight / cellSize) * cellSize;
 };
 
-const resetCanvas = (canvas, bgColor, cellSpacing) => {
+const resetCanvas = (canvas, backgroundColor, cellMargin) => {
   const ctx = canvas.getContext("2d");
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = bgColor;
+  ctx.fillStyle = backgroundColor;
   ctx.fillRect(0, 0, canvas.width, canvas.height);
-  canvas.style.border = `${cellSpacing}px solid ${bgColor}`;
+  canvas.style.border = `${cellMargin}px solid ${backgroundColor}`;
 };
 
-const paintCells = (canvas, activeCells, gridSize, color, spacing) => {
+const paintCells = (canvas, activeCells, size, color, margin) => {
   const ctx = canvas.getContext("2d");
-  const cellW = canvas.width / gridSize;
-  const cellH = canvas.height / gridSize;
 
   ctx.fillStyle = color;
   for (let cell of activeCells) {
     const [col, row] = cell.split("-");
     ctx.fillRect(
-      cellW * col + spacing,
-      cellH * row + spacing,
-      cellW - 2 * spacing,
-      cellH - 2 * spacing
+      size * col + margin,
+      size * row + margin,
+      size - 2 * margin,
+      size - 2 * margin
     );
   }
 };
 
 const drawScene = (options, activeCells) => {
-  const { canvas, gridSize, backgroundColor, cellColor, cellSpacing } = options;
-  resetCanvas(canvas, backgroundColor, cellSpacing);
-  paintCells(canvas, activeCells, gridSize, cellColor, cellSpacing);
+  const { canvas, backgroundColor, cellSize, cellColor, cellMargin } = options;
+  resetCanvas(canvas, backgroundColor, cellMargin);
+  paintCells(canvas, activeCells, cellSize, cellColor, cellMargin);
 };
 
-const countNeighbours = (activeCells, gridSize, x, y) => {
+const countNeighbours = (activeCells, gridCols, gridRows, x, y) => {
   const neighbours = [
     [-1, -1],
     [0, -1],
@@ -48,19 +47,19 @@ const countNeighbours = (activeCells, gridSize, x, y) => {
   let count = 0;
   for (const n of neighbours) {
     const [nX, nY] = [x + n[0], y + n[1]];
-    if (nX < 0 || nX >= gridSize || nY < 0 || nY >= gridSize) continue;
+    if (nX < 0 || nX >= gridCols || nY < 0 || nY >= gridRows) continue;
     if (activeCells.has(`${nX}-${nY}`)) count += 1;
   }
   return count;
 };
 
-const nextActiveCells = (activeCells, gridSize) => {
+const nextActiveCells = (activeCells, gridCols, gridRows) => {
   let result = new Set();
 
-  for (let y = 0; y < gridSize; y++) {
-    for (let x = 0; x < gridSize; x++) {
+  for (let y = 0; y < gridRows; y++) {
+    for (let x = 0; x < gridCols; x++) {
       const liveCell = activeCells.has(`${x}-${y}`);
-      const neighbours = countNeighbours(activeCells, gridSize, x, y);
+      const neighbours = countNeighbours(activeCells, gridCols, gridRows, x, y);
       if ((liveCell && [2, 3].includes(neighbours)) || neighbours === 3) {
         result.add(`${x}-${y}`);
       }
@@ -70,27 +69,35 @@ const nextActiveCells = (activeCells, gridSize) => {
 };
 
 const play = (options) => {
-  const { gridSize, cellColor, speed } = options;
+  const { canvas, cellSize, speed } = options;
   let { activeCells } = options;
   const delay = Math.max(1000 / speed, 100);
 
-  initPage(cellColor);
-  drawScene(options, activeCells);
+  stretchCanvas(canvas, cellSize);
+  drawScene(options, cells);
+
+  const [gridCols, gridRows] = [
+    canvas.width / cellSize,
+    canvas.height / cellSize,
+  ];
 
   setInterval(() => {
-    activeCells = nextActiveCells(activeCells, gridSize);
+    activeCells = nextActiveCells(activeCells, gridCols, gridRows);
     drawScene(options, activeCells);
   }, delay);
 };
 
+const canvas = document.querySelector("canvas#grid");
+
 const options = {
-  canvas: document.querySelector("canvas#grid"),
-  gridSize: 5, // the number of cells across and down (assumes a square grid)
-  cellSpacing: 3, // spacing in pixels around each cell
+  canvas,
+  cellSize: 10, // block size in pixels
+  cellMargin: 1, // margin in pixels around each cell
   cellColor: "black",
   backgroundColor: "white",
-  speed: 1, // setting from 1-10
-  activeCells: new Set(["1-2", "2-2", "3-2"]),
+  speed: 10, // setting from 1-10
+  activeCells: new Set(["2-1", "3-2", "1-3", "2-3", "3-3"]),
 };
 
+window.addEventListener("resize", () => stretchCanvas(canvas));
 play(options);
